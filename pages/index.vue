f78d9859bbc96cf4edcee5f2a4141f6cac8762da
yonas sisay <yonimelkamu357@gmail.com>
2022-08-30 00:17:21 +0300

    nuxt starter kit

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container mx-auto bg-gray-300 p-8">
+    <h1 class="font-bold text-gray-600 text-lg">Hello, Tailwind 3!</h1>
+    <button class="btn btn-primary">button</button>
+  </div>
+</template>
